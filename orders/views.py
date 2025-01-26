524f133392f95a0f9d1cc27a44d23742f2af85c6
andrewindeche <indecheandrew@gmail.com>
2025-01-26 21:52:15 +0300

    add views

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.shortcuts import render,get_object_or_404
+from django.http import JsonResponse
+from rest_framework import generics
+from .models import Order, Customer
+from .serializers import OrderSerializer
+from .utils import send_sms_alert
 
 # Create your views here.
+@api_view(['POST'])
+def create_order(request):
+    customer_code = request.data.get('customer_code')
+    customer = get_object_or_404(Customer, code=customer_code)
+    item = request.data.get('item')
+    amount = request.data.get('amount')
+
+    order = Order.objects.create(customer=customer, item=item, amount=amount)
+
+    # Send SMS alert
+    send_sms_alert(customer, order, 'created')
+
+    return JsonResponse({'message': 'Order created successfully'})
+
+class UpdateOrderView(generics.UpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def perform_update(self, serializer):
+        order = serializer.save()
+        customer = order.customer
+        send_sms_alert(customer, order, 'updated')
