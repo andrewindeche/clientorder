62a58f08eefabd974b700920dad1225fe8eb8be5
andrewindeche <indecheandrew@gmail.com>
2025-01-26 21:53:53 +0300

    add africastalking utils

---
+++ utils.py
@@ -0,0 +1,16 @@
+# orderapp/utils.py
+
+import africastalking
+from django.conf import settings
+
+def send_sms_alert(customer, order, action='placed'):
+    username = "sandbox"
+    api_key = settings.AFRICASTALKING_API_KEY
+    africastalking.initialize(username, api_key)
+
+    sms = africastalking.SMS
+    message = f"Order for {order.item} has been {action}. Amount: {order.amount}. Time: {order.time}"
+    recipient = customer.phone  
+
+    response = sms.send(message, [recipient])
+    print(response)
