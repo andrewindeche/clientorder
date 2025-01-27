8e92a08731d11fed969c7db883eebcc02f0413a3
andrewindeche <indecheandrew@gmail.com>
2025-01-27 08:44:07 +0300

    add account authentications urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .views import create_order, UpdateOrderView
 
 urlpatterns = [
+    path('accounts/', include('allauth.urls')),
     path('create_order/', create_order, name='create_order'),
     path('update_order/<int:pk>/', UpdateOrderView.as_view(), name='update_order'),
 ]
