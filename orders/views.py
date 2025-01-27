35ad922128e95e8dc488041a4036106bfdad7b70
andrewindeche <indecheandrew@gmail.com>
2025-01-27 09:40:15 +0300

    protect routes

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, permission_classes
 from django.shortcuts import render,get_object_or_404
 from django.http import JsonResponse
 from rest_framework import generics
@@ -8,6 +10,7 @@ from .utils import send_sms_alert
 
 # Create your views here.
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def create_order(request):
     customer_code = request.data.get('customer_code')
     customer = get_object_or_404(Customer, code=customer_code)
@@ -23,6 +26,7 @@ def create_order(request):
 class UpdateOrderView(generics.UpdateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_update(self, serializer):
         order = serializer.save()
