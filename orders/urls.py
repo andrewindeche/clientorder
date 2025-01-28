143613b9bc298befdab7836e4d3c535351097add
andrewindeche <indecheandrew@gmail.com>
2025-01-28 09:21:19 +0300

    update urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 from .views import create_order, UpdateOrderView
 
 urlpatterns = [
     path('create_order/', create_order, name='create_order'),
     path('update_order/<int:pk>/', UpdateOrderView.as_view(), name='update_order'),
+    path('account_page/', views.account_page, name='account_page'),
 ]
