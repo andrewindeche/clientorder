44babf814a677f1c63140d16ac878b454f6fc746
andrewindeche <indecheandrew@gmail.com>
2025-01-28 13:28:46 +0300

    update view and remove redundant imports

--- orders/views.py
+++ orders/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import redirect
 from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 from django.shortcuts import render,get_object_or_404
 from django.http import JsonResponse
 from rest_framework import generics
 from .models import Order, Customer
 from .serializers import OrderSerializer
 from .utils import send_sms_alert
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 @api_view(['POST'])
@@ -44,10 +47,6 @@ def view_customer_code(request):
     except Customer.DoesNotExist:
         return JsonResponse({'error': 'Customer does not exist'}, status=404)
 
-from django.contrib.auth import login, authenticate
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-
 @login_required
 def login_view(request):
     if request.method == 'POST':
@@ -59,20 +58,36 @@ def login_view(request):
             return redirect('account_page') 
         else:
             return render(request, 'login.html', {'error': 'Invalid login credentials'})
+        
+def signup_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            login(request, user)
+            return redirect('account_page')
+    else:
+        form = CustomUserCreationForm()
 
-
+    print(form)
+    return render(request, 'signup.html', {'form': form})
     
 @login_required
 def account_page(request):
-    user = request.user
-    try:
-        customer = Customer.objects.get(user=user)
-        customer_code = customer.code
-    except Customer.DoesNotExist:
-        customer_code = None 
+    customer = Customer.objects.get(user=request.user)
+    phone_updated = False
+    
+    if request.method == 'POST':
+        phone = request.POST.get('phone')
+        customer.phone = phone
+        customer.save()
+        phone_updated = True
+        return redirect('account_page')
 
     context = {
-        'customer_code': customer_code,
+        'customer_code': customer.code,
+        'customer': customer,
+        'phone_updated': phone_updated,
     }
     return render(request, 'accounts/account_page.html', context)
-
