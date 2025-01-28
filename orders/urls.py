984658e491684b0727a6c4355d2a09af8fcb3d97
andrewindeche <indecheandrew@gmail.com>
2025-01-28 13:26:41 +0300

    update urls

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('create_order/', create_order, name='create_order'),
     path('update_order/<int:pk>/', UpdateOrderView.as_view(), name='update_order'),
     path('account_page/', views.account_page, name='account_page'),
+    path('signup/', views.signup_view, name='register'),
+    path('update-phone/', views.account_page, name='update_phone'), 
 ]
