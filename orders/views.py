7d51ff3a9f1ce8334f5df75fb84dd5f431b14048
andrewindeche <indecheandrew@gmail.com>
2025-01-28 09:21:59 +0300

    Add login template and view

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,8 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import SessionAuthentication
+from django.shortcuts import redirect
 from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 from django.shortcuts import render,get_object_or_404
 from django.http import JsonResponse
 from rest_framework import generics
@@ -32,3 +34,45 @@ class UpdateOrderView(generics.UpdateAPIView):
         order = serializer.save()
         customer = order.customer
         send_sms_alert(customer, order, 'updated')
+
+@login_required
+def view_customer_code(request):
+    user = request.user
+    try:
+        customer = Customer.objects.get(user=user)
+        return JsonResponse({'customer_code': customer.code})
+    except Customer.DoesNotExist:
+        return JsonResponse({'error': 'Customer does not exist'}, status=404)
+
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('account_page') 
+        else:
+            return render(request, 'login.html', {'error': 'Invalid login credentials'})
+
+
+    
+@login_required
+def account_page(request):
+    user = request.user
+    try:
+        customer = Customer.objects.get(user=user)
+        customer_code = customer.code
+    except Customer.DoesNotExist:
+        customer_code = None 
+
+    context = {
+        'customer_code': customer_code,
+    }
+    return render(request, 'accounts/account_page.html', context)
+
