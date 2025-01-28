781b2fc7b9c24bd8b6085b71bd5478ce500e17f6
andrewindeche <indecheandrew@gmail.com>
2025-01-28 13:27:22 +0300

    add error handling

--- orders/utils.py
+++ orders/utils.py
@@ -2,13 +2,18 @@ import africastalking
 from django.conf import settings
 
 def send_sms_alert(customer, order, action='placed'):
-    username = "sandbox"
+    username = "sandbox" 
     api_key = settings.AFRICASTALKING_API_KEY
     africastalking.initialize(username, api_key)
 
     sms = africastalking.SMS
-    message = f"Order for {order.item} has been {action}. Amount: {order.amount}. Time: {order.time}"
-    recipient = customer.phone  
-
-    response = sms.send(message, [recipient])
-    print(response)
+    message = f"Order for {order.item} has been {action}. Amount: {order.amount}. Time: {order.time.strftime('%Y-%m-%d %H:%M:%S')}"
+    
+    recipient = customer.phone 
+    
+    try:
+        response = sms.send(message, [recipient])
+        print(f"SMS sent successfully to {recipient}")
+        print(f"Response: {response}")
+    except Exception as e:
+        print(f"Error sending SMS: {e}")
