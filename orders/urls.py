0b0b2b6b6a7e6f691e975edfe06ea30481791367
andrewindeche <indecheandrew@gmail.com>
2025-01-28 23:42:49 +0300

    Add login url

--- orders/urls.py
+++ orders/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('create_order/', create_order, name='create_order'),
     path('account_page/', views.account_page, name='account_page'),
     path('signup/', views.signup_view, name='register'),
+    path('login/', views.login_view, name='login'),
     path('update-phone/', views.account_page, name='update_phone'), 
     path('update_order/<uuid:order_id>/', UpdateOrderView.as_view(), name='update_order'),
 ]
