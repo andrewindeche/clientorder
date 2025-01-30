df299683ea0a1673aa0e477ac5b66328e10b0a99
andrewindeche <indecheandrew@gmail.com>
2025-01-30 09:30:22 +0300

    update mutation

--- clientorderservice/mutations.py
+++ clientorderservice/mutations.py
@@ -1,8 +1,19 @@
+import os
 import graphene
 from orders.models import Order, Customer
 import requests
+from graphene_django import DjangoObjectType
 from django.conf import settings
+from graphql_jwt.decorators import login_required
+from django.shortcuts import get_object_or_404
 from .types import OrderType
+from orders.utils import send_sms_alert 
+import africastalking
+
+africastalking.initialize(username=os.getenv('AFRICASTALKING_USERNAME'), 
+api_key=os.getenv('AFRICASTALKING_API_KEY'))
+sms = africastalking.SMS
+
 
 class GenerateToken(graphene.Mutation):
     access = graphene.String()
@@ -41,19 +52,46 @@ class RefreshToken(graphene.Mutation):
         else:
             raise Exception("Invalid refresh token")
 
+def send_sms_alert(customer, order, action):
+    message = f"Dear {customer.name}, your order for {order.item} has been {action}."
+    recipients = [customer.phone]
+    sms.send(message, recipients)
+
+class OrderType(DjangoObjectType):
+    class Meta:
+        model = Order
+
+class CreateOrderInput(graphene.InputObjectType):
+    customer_code = graphene.String(required=True)
+    item = graphene.String(required=True)
+    amount = graphene.Float(required=True)
+
 class CreateOrder(graphene.Mutation):
     class Arguments:
-        customer_code = graphene.String(required=True)
-        item = graphene.String(required=True)
-        amount = graphene.Decimal(required=True)
+        input = CreateOrderInput(required=True)
+    
+    order = graphene.Field(OrderType)
+    message = graphene.String()
 
-    order = graphene.Field(lambda: OrderType)
+    @login_required 
+    def mutate(self, info, input):
+        user = info.context.user 
+        customer_code = input.customer_code
+        customer = get_object_or_404(Customer, code=customer_code)
+        item = input.item
+        amount = input.amount
 
-    def mutate(self, info, customer_code, item, amount):
-        customer = Customer.objects.get(code=customer_code)
+        if not item or not amount:
+            return CreateOrder(order=None, message='Item and amount are required fields')
+
         order = Order.objects.create(customer=customer, item=item, amount=amount)
-        return CreateOrder(order=order)
+        send_sms_alert(customer, order, 'created')
 
+        return CreateOrder(order=order, message='Order created successfully')
+
+class Mutation(graphene.ObjectType):
+    create_order = CreateOrder.Field()
+    
 class UpdateOrder(graphene.Mutation):
     class Arguments:
         order_id = graphene.UUID(required=True)
@@ -76,3 +114,4 @@ class Mutation(graphene.ObjectType):
     refresh_token = RefreshToken.Field()
     create_order = CreateOrder.Field()
     update_order = UpdateOrder.Field()
+
