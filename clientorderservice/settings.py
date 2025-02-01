34f4315c2bb7d3b31de55dbb9a57e0b6e2c66c36
andrewindeche <indecheandrew@gmail.com>
2025-02-01 21:50:03 +0300

    update base url to variables

--- clientorderservice/settings.py
+++ clientorderservice/settings.py
@@ -191,7 +191,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
-BASE_URL = 'http://localhost:8000'
+BASE_URL = os.environ.get('DATABASE_USER'),
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_HOST = 'smtp.gmail.com'
 #EMAIL_PORT = 587
